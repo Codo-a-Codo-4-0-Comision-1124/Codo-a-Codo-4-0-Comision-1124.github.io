bafaf20e33720ad515ff381b74b8c089ec49e2bd
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2021-10-05 20:36:53 -0300

    last changes of the day

--- js/tests.js
+++ js/tests.js
@@ -26,11 +26,37 @@ let miSuperVariable = Number(100);
 
 otraVariable = miSuperVariable;
 
-var myInput = prompt("Ingrese su edad..");
+var myInput = 0;//prompt("Ingrese su edad..");
 // Ojo con los comentarios en una app en produccion..
 console.log(miVariableString , miSuperVariable, myInput);
 
-
 let ultimaVariable = myInput - miSuperVariable;
 
-console.log(ultimaVariable);
+console.log(ultimaVariable); // console es un objeto que nos permite acceder a la consola
+
+var myPrimitiveString = "Hello world"
+var myObjectString = String("Hola mundo");
+var mySecondString = new String("Hi World");
+
+let posInit = 2;
+let amountChars = 6;
+
+// En Javascript no necesitamos un orden para llamar la funcion antes de definirla
+documentWriteH1(myPrimitiveString.toUpperCase(), "strong"); 
+documentWriteH1(myPrimitiveString, "h1"); 
+documentWriteH1(myPrimitiveString, "h2"); 
+documentWriteH1(myObjectString.substr(posInit, amountChars), "span"); 
+documentWriteH1(mySecondString.toUpperCase(), "div"); 
+
+
+/**
+ * Esta funcion escribe en el document con 
+ * Los parametros que se les pasaron...
+ * @param {String} parameters 
+ */
+ function documentWriteH1(parameters, tag) {
+    console.log(tag);
+    document.write(`<${tag}>`);// Abro etiqueta
+    document.write(parameters);
+    document.write(`</${tag}>`); // cierro etiqueta
+}
